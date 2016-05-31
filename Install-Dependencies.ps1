42efa7a84298a187a3de95c8acbd1d28da61a3ef
Andrew Arnott <andrewarnott@gmail.com>
2016-05-31 09:00:44 -0700

    Skip installing Azure Storage emulator when preinstalled

--- Install-Dependencies.ps1
+++ Install-Dependencies.ps1
@@ -1,13 +1,18 @@
-$AzureStorageEmulatorInstallerPath = "$env:temp\AzureEmulatorInstaller.msi"
-if (!(Test-Path $AzureStorageEmulatorInstallerPath)) {
-    Write-Host "Downloading the Azure Storage Emulator" -ForegroundColor Green
-    Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=717179&clcid=0x409" -OutFile $AzureStorageEmulatorInstallerPath
-    Unblock-File $AzureStorageEmulatorInstallerPath
+$AzureStorageEmulator = "${env:ProgramFiles(x86)}\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe"
+
+if (!(Test-Path $AzureStorageEmulator)) {
+    $AzureStorageEmulatorInstallerPath = "$env:temp\AzureEmulatorInstaller.msi"
+    if (!(Test-Path $AzureStorageEmulatorInstallerPath)) {
+        Write-Host "Downloading the Azure Storage Emulator..." -ForegroundColor Green
+        Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=717179&clcid=0x409" -OutFile $AzureStorageEmulatorInstallerPath
+        Unblock-File $AzureStorageEmulatorInstallerPath
+    }
+
+    Write-Host "Installing the Azure Storage Emulator..." -ForegroundColor Green
+    Start-Process -FilePath msiexec -ArgumentList '/i',"$AzureStorageEmulatorInstallerPath","/passive","/norestart" -Wait
+    Write-Host "Initializing the Azure Storage Emulator..." -ForegroundColor Green
+    & $AzureStorageEmulator init -inprocess
 }
 
-Write-Host "Installing the Azure Storage Emulator..." -ForegroundColor Green
-Start-Process -FilePath msiexec -ArgumentList '/i',"$AzureStorageEmulatorInstallerPath","/passive","/norestart" -Wait
-$AzureStorageEmulator = "${env:ProgramFiles(x86)}\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe"
 Write-Host "Starting the Azure Storage Emulator..." -ForegroundColor Green
-& $AzureStorageEmulator init -inprocess
 & $AzureStorageEmulator start
